7172fa77adab6f94d525c5ae13518a5329bd5a63
CJacobs <cjacobs3971@gmail.com>
2023-07-09 15:08:34 -0700

    complete models for category and index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foriegnkey: 'ProductTag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foriegnkey: 'ProductTag'
+});
 
 module.exports = {
   Product,
